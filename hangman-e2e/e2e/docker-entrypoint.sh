b707228a08edc0b050259331066cfb0c21fa6c91
monicacrespo <moni.crespo@gmail.com>
2024-04-18 21:21:52 +0100

    Removed API_URL from e2e service in docker compose

--- hangman-e2e/e2e/docker-entrypoint.sh
+++ hangman-e2e/e2e/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # Abort on any error (including if wait-for-it fails).
 set -e
 
-# Wait for the backend to be up, if we know where it is.
+# Wait for the frontend to be up, if we know where it is.
 if [ -n "$HANGMAN_FRONT_HOST" ]; then
   /app/wait-for-it.sh "$HANGMAN_FRONT_HOST:${HANGMAN_FRONT_PORT:-8080}"
 fi
